26f356b6f501018314633e307539df8af1eff02a
Jai <814683@qq.com>
2025-02-09 22:56:57 +0800

    feat: NPC对话框自动处理逻辑

--- Modules/Misc/Gossip.lua
+++ Modules/Misc/Gossip.lua
@@ -26,12 +26,41 @@ local function GossipFrameOnKeyDown(self, key)
     end
 end
 
+local ICON = {
+    [132050] = true, -- 银行
+    [132057] = true, -- 鸟点
+    [132058] = true, -- 专业技能
+    [132060] = true, -- 商店
+    [528409] = true -- 拍卖行
+}
+local function FindChoice(options)
+    if #options <= 1 then
+        return options[1]
+    end
+    for _, opt in ipairs(options) do
+        if opt.name == 'battlemaster' or ICON[opt.icon] then
+            return opt
+        end
+    end
+end
+
+local function GossipOnShow(self)
+    if not IsShiftKeyDown() and #C_GossipInfo.GetActiveQuests() == 0 and #C_GossipInfo.GetAvailableQuests() == 0 then
+        local opt = FindChoice(C_GossipInfo.GetOptions())
+        if opt then
+            C_GossipInfo.SelectOption(opt.gossipOptionID)
+        end
+    end
+end
+
 if GossipFrame then
-    Module:Add('misc-gossip-hotkey', true, L['Gossip Hotkey'], L['Enable hotkey for Gossip Frame'], function(self, enable)
+    Module:Add('misc-gossip-autoit', true, L['Gossip AutoIt'], L['Enable AutoIt for Gossip Frame'], function(self, enable)
         if enable then
             GossipFrame:SetScript('OnKeyDown', GossipFrameOnKeyDown)
+            self:Event('GOSSIP_SHOW', GossipOnShow)
         else
             GossipFrame:SetScript('OnKeyDown', nil)
+            self:UnEvent('GOSSIP_SHOW', GossipOnShow)
         end
     end)
 end
