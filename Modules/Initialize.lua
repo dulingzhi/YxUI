5d10f8092fe2c405fb826e3d6dda8efc342301c8
Jai <814683@qq.com>
2025-11-19 23:33:38 +0800

    feat: 兼容时光服

--- Modules/Initialize.lua
+++ Modules/Initialize.lua
@@ -42,7 +42,7 @@ Y.IsMaxLevel = Y.UserLevel == GetMaxLevelForExpansionLevel(GetExpansionLevel())
 Y.ClientVersion = select(4, GetBuildInfo())
 Y.IsClassic = Y.ClientVersion > 10000 and Y.ClientVersion < 20000
 Y.IsTBC = Y.ClientVersion > 20000 and Y.ClientVersion < 30000
-Y.IsWrath = false -- Y.ClientVersion > 30000 and Y.ClientVersion < 40000
+Y.IsWrath = Y.ClientVersion > 30000 and Y.ClientVersion < 40000
 Y.IsCata = Y.ClientVersion > 30000 and Y.ClientVersion < 50000
 Y.IsMists = Y.ClientVersion > 50000 and Y.ClientVersion < 60000
 Y.IsMainline = Y.ClientVersion > 90000
